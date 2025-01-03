bbfcbc9174196b275f55a8355427670f6cb6fa47
Juli <juli-felix@outlook.com>
2025-01-03 00:44:32 -0300

    First commit

---
+++ src/app/square/square.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+import { NgForOf } from '@angular/common';
+
+@Component({
+  selector: 'ttt-square',
+  imports: [NgForOf],
+  template: `<button>{{ value }}</button>`,
+  styleUrl: './square.component.css'
+})
+export class SquareComponent {
+  @Input() value!: 'X' | 'O'; 
+
+  
+
+  constructor(){
+    
+  }
+  
+}
